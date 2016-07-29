536887a6311efd9f7b7f689bf2c8ce6e02649cb8
James Hwang <jameshwang2289@gmail.com>
2016-07-29 14:20:54 -0500

    added pass through vert and frag shaders

--- view/islandAndMovement.py
+++ view/islandAndMovement.py
@@ -3,9 +3,11 @@ from cyclops import *
 from pointCloud import *
 from math import *
 
-def createCustomGeom(f, scene, color, geomName):        #Function parses file and creates lines
+def createCustomGeom(f, scene, geomName):        #Function parses file and creates lines
                                                         #that represent movement into a single
                                                         #custom shape.
+    global moveLineProgram
+
     firstRun = True
 
     numVertices = 0
@@ -19,7 +21,7 @@ def createCustomGeom(f, scene, color, geomName):        #Function parses file an
     unitY = Vector3(0,1,0)
     unitZ = Vector3(0,0,1)
 
-    thickness = 1.5
+    thickness = 1
 
     geom = ModelGeometry.create(geomName)
     for line in f:
@@ -36,12 +38,11 @@ def createCustomGeom(f, scene, color, geomName):        #Function parses file an
         tokens2 = line2.split(" ")
         if firstRun:
             tokens = line.split(" ")
-            dayCounter = tokens[4]
-            nodeCounter = 1
         else:
             tokens = prevLine.split(" ")
 
-        pos1 = Vector3(float(tokens[0]), float(tokens[1]), float(tokens[2]))
+        if firstRun:
+            pos1 = Vector3(float(tokens[0]), float(tokens[1]), float(tokens[2]))
         
         pos2 = Vector3(float(tokens2[0]), float(tokens2[1]), float(tokens2[2]))
 
@@ -49,6 +50,7 @@ def createCustomGeom(f, scene, color, geomName):        #Function parses file an
         d = vec.normalize()
         unitZV1 = d.cross(unitZ)
         unitYV1 = d.cross(unitY)
+
         if firstRun:
             v1 = pos1+thickness*unitZV1+thickness*unitYV1       #list of vertices
             v2 = pos1+thickness*unitZV1-thickness*unitYV1
@@ -70,66 +72,77 @@ def createCustomGeom(f, scene, color, geomName):        #Function parses file an
         v7 = pos2-thickness*unitZV1+thickness*unitYV1
         v8 = pos2-thickness*unitZV1-thickness*unitYV1
 
+        dayDelta = int(tokens2[3])
+        hr = int(tokens2[4])
+        minute = int(tokens2[5])
+        individualID = int(tokens2[6])
+
+        color = []
+        color.append(int(tokens2[3]))
+        color.append(int(tokens2[4]))
+        color.append(int(tokens2[5]))
+        color.append(int(tokens2[6]))
+
     #####################Front Panel##################################################
         geom.addVertex( v3 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v2 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v1 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
 
         geom.addVertex( v2 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v3 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v4 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
     ##################################################################################
     #####################Back Panel##################################################
         geom.addVertex( v7 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v6 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v5 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
 
         geom.addVertex( v6 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v7 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v8 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
     ##################################################################################
     #####################Top Panel##################################################
         geom.addVertex( v7 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v1 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v5 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         
         geom.addVertex( v7 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v3 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v1 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
     ##################################################################################
     #####################Bottom Panel##################################################
         
         geom.addVertex( v2 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v6 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v4 )
-        geom.addColor(Color(color)) 
+        geom.addColor(Color(color[0], color[1], color[2], color[3])) 
 
         geom.addVertex( v6 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         geom.addVertex( v4 )
-        geom.addColor(Color(color))    
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))    
         geom.addVertex( v8 )
-        geom.addColor(Color(color))
+        geom.addColor(Color(color[0], color[1], color[2], color[3]))
         
     ##################################################################################
         
@@ -150,11 +163,11 @@ def createCustomGeom(f, scene, color, geomName):        #Function parses file an
     scene.addModel(geom)
     obj = StaticObject.create(geomName)
     obj.setPosition(0, 0, 0)
-    obj.setEffect('colored -e blue -C -t -a')
+    # obj.setEffect('colored -e blue -C -t -a')
 
-    obj.getMaterial().setProgram('colored byvertex-emissive')
+    # obj.getMaterial().setProgram('colored byvertex-emissive')
     obj.getMaterial().setTransparent(True)
-    obj.getMaterial().setAlpha(0.75)
+    obj.getMaterial().setAlpha(1.0)
     print 'finished Parsing'
 
     return obj 
@@ -224,98 +237,64 @@ getDefaultCamera().setPosition(imgResRatioX*10260/2, imgResRatioY*9850/2, 2500)
 # startDay = Uniform.create('startDay', UniformType.Int, 1)
 # endDay = Uniform.create('endDay', UniformType.Int, 1)
 
-# myStartDay = 0
-# myEndDay = 1
-# dayIncrement = 1
-# numberOfDays = 84
+myStartDay = 0
+myEndDay = 1
+dayIncrement = 1
+numberOfDays = 84
 currentPitch = 0
 currentYaw = 0
 currentRoll = 0
 
-# startDay.setInt(myStartDay)
-# endDay.setInt(myEndDay)
-
-# colorBy = Uniform.create('colorBy', UniformType.Int, 1) #if 1, shaders turn on.  If 0, shaders turn off
-# colorBy.setInt(0)
-
-# selectedIndividual1 = Uniform.create('selectedIndividual1', UniformType.Int, 1) #if 1, shaders turn on.  If 0, shaders turn off
-# selectedIndividual1.setInt(4693)
-
-# selectedIndividual2 = Uniform.create('selectedIndividual2', UniformType.Int, 1) #if 1, shaders turn on.  If 0, shaders turn off
-# selectedIndividual2.setInt(4693)
-
-# movePointScale = Uniform.create('movePointScale', UniformType.Float, 1)
-# movePointScale.setFloat(8.0)
-
-# #Point cloud created here- makes sure it is different name from James point cloud
-# movePointProgram = ProgramAsset()
-# movePointProgram.name = "movePoints"
-# movePointProgram.vertexShaderName = "movementShaders/Sphere.vert" #here are our shaders
-# movePointProgram.fragmentShaderName = "movementShaders/Sphere.frag"
-# movePointProgram.geometryShaderName = "movementShaders/mySphere.geom"
-# movePointProgram.geometryOutVertices = 4
-# movePointProgram.geometryInput = PrimitiveType.Points
-# movePointProgram.geometryOutput = PrimitiveType.TriangleStrip
-# scene.addProgram(movePointProgram)
-
-# movePointCloudModel = ModelInfo()
-# movePointCloudModel.name = 'movePointCloud'
-# movePointCloudModel.path = 'allChibi.xyzb'#'XY_Chibi_Christmas_Parsed.xyzb'#'Chibi_Christmas_Parsed.xyzb' #'newpng.xyzb'
-# #movePointCloudModel.options = "10000 100:1000000:5 20:100:4 6:20:2 0:5:1"
-# movePointCloudModel.options = "10000 100:1000000:20 20:100:10 6:20:5 0:5:5"
-# #movePointCloudModel.options = "10000 0:1000000:1"
-# scene.loadModel(movePointCloudModel)
-
-# movePointCloud = StaticObject.create(movePointCloudModel.name)
-# # attach shader uniforms
-# moveMat = movePointCloud.getMaterial()
-# moveMat.setProgram(movePointProgram.name)
-
-# moveMat.attachUniform(movePointScale)
-# moveMat.attachUniform(startDay)
-# moveMat.attachUniform(endDay)
-# moveMat.attachUniform(selectedIndividual1)
-# moveMat.attachUniform(selectedIndividual2)
-# moveMat.attachUniform(colorBy)
-
-
-#CPU Version
-# movement = open("parsedAllChibi.txt", "r")
-# thickness3 = 8
-# c3 = LineSet.create()
-# moveList = []
-# moveIndex = 0
-
-# for line in movement:
-#     tokens = line.split(" ")
-#     moveList.append([])
-#     moveList[moveIndex].append(tokens[0])
-#     moveList[moveIndex].append(tokens[1])
-#     moveList[moveIndex].append(tokens[2])
-#     moveIndex += 1
-
-# for i in range(0,moveIndex):
-#     if (i+1 < moveIndex):
-#         l3 = c3.addLine()
-#         l3.setStart(Vector3(float(moveList[i][0]), float(moveList[i][1]), float(moveList[i][2])))
-#         l3.setEnd(Vector3(float(moveList[i+1][0]), float(moveList[i+1][1]), float(moveList[i+1][2])))
-#         l3.setThickness(thickness3)
-#         c3.setEffect('colored -e blue')
-# movement.close()
-
 #Cylinder and Sphere Version
+
+moveLineProgram = ProgramAsset()
+moveLineProgram.name = "moveLine"
+moveLineProgram.vertexShaderName = "lineShaders/Line.vert"
+moveLineProgram.fragmentShaderName = "lineShaders/Line.frag"
+scene.addProgram(moveLineProgram)
+
+startDay = Uniform.create('startDay', UniformType.Int, 1)
+startDay.setInt(0)
+endDay = Uniform.create('endDay', UniformType.Int, 1)
+endDay.setInt(1)
+colorBy = Uniform.create('colorBy', UniformType.Int, 1)
+colorBy.setInt(0)
+selectedIndividual1 = Uniform.create('selectedIndividual1', UniformType.Int, 1)
+selectedIndividual1.setInt(4693)
+selectedIndividual2 = Uniform.create('selectedIndividual2', UniformType.Int, 1) #if 1, shaders turn on.  If 0, shaders turn off
+selectedIndividual2.setInt(4693)
 
 f = open("gpsMovement/Chibi.txt", "r")
 
-chibi = createCustomGeom(f, scene, 'blue', 'chibi')
+chibi = createCustomGeom(f, scene, 'chibi')             #Ateles geoffroyi
 
-f = open("gpsMovement/Abby.txt", "r")
 
-abby = createCustomGeom(f, scene, 'purple', 'abby')
+chibiMat = chibi.getMaterial()
+chibiMat.setProgram(moveLineProgram.name)
+chibiMat.attachUniform(startDay)
+chibiMat.attachUniform(endDay)
+chibiMat.attachUniform(colorBy)
+chibiMat.attachUniform(selectedIndividual1)
+chibiMat.attachUniform(selectedIndividual2)
 
-f = open("gpsMovement/Clementina.txt", "r")
 
-clementina = createCustomGeom(f, scene, 'red', 'clementina')
+# f = open("gpsMovement/Abby.txt", "r")
+
+# abby = createCustomGeom(f, scene, 'abby')             #Potos flavus
+
+# abbyMat = abby.getMaterial()
+# abbyMat.setProgram(moveLineProgram.name)
+# abbyMat.attachUniform(startDay)
+# abbyMat.attachUniform(endDay)
+
+# f = open("gpsMovement/Clementina.txt", "r")
+
+# clementina = createCustomGeom(f, scene, 'clementina')    #Nasua narica
+
+# clemMat = clementina.getMaterial()
+# clemMat.setProgram(moveLineProgram.name)
+# clemMat.attachUniform(startDay)
+# clemMat.attachUniform(endDay)
 
 #-----------------------------------------------------------------------------
 #Terrain code
@@ -409,47 +388,25 @@ vbtn = ss.addButton("Vertical View", "viewVertical(1)")
 hbtn = ss.addButton("Horizontal View", "viewHorizontal(1)")
 
 #------------------------------------------------------------------------------
-#CPU Submenu Items
-ss2 = mm.getMainMenu().addSubMenu("Selected Individual")
-subMenuChibi = ss2.addSubMenu("Chibi 4693")
-#subMenuChibi.setStyleValue('fill', '#954FEA')
-subMenuChibi.addLabel("# of Days to Show")
-daySliderChibi = subMenuChibi.addSlider(4, "setDaySize(%value%)")
-daySliderC = daySliderChibi.getSlider()
-daySliderC.setValue(1)
-
-subMenuChibi.addButton("Show Next Day(s)", "showNextDay(4693)")
-subMenuChibi.addButton("Show Previous Day(s)", "showPrevDay(4693)")
-
-subMenuAbby = ss2.addSubMenu("Abby 4652")
-subMenuAbby.addLabel("# of Days to Show")
-daySliderAbby = subMenuAbby.addSlider(4, "setDaySize(%value%)")
-daySliderA = daySliderAbby.getSlider()
-daySliderA.setValue(1)
-
-subMenuAbby.addButton("Show Next Day(s)", "showNextDay(4652)")
-subMenuAbby.addButton("Show Previous Day(s)", "showPrevDay(4652)")
-
-#------------------------------------------------------------------------------
 #GPU Submenu Items
 
 # #SUBMENU STEPTHRO
-# ss2 = mm.getMainMenu().addSubMenu("Step Through Options")
-# btnOneUp = ss2.addButton("Forward a day", "oneDayStepUp(1)")
-# btnOneDown = ss2.addButton("Backward a day", "oneDayStepDown(1)")
-# btnSvnUp = ss2.addButton("7 Days Forward", "sevenDayStepUp(1)")
-# btnSvnUp = ss2.addButton("7 Days Backward", "sevenDayStepDown(1)")
-# ss2.addLabel("--------------------")
-# btnAll = ss2.addButton("All Days", "allDay(1)")
+ss2 = mm.getMainMenu().addSubMenu("Step Through Options")
+btnOneUp = ss2.addButton("Forward a day", "oneDayStepUp(1)")
+btnOneDown = ss2.addButton("Backward a day", "oneDayStepDown(1)")
+btnSvnUp = ss2.addButton("7 Days Forward", "sevenDayStepUp(1)")
+btnSvnUp = ss2.addButton("7 Days Backward", "sevenDayStepDown(1)")
+ss2.addLabel("--------------------")
+btnAll = ss2.addButton("All Days", "allDay(1)")
 
 # #SUBMENU COLOR
-# ss3 = mm.getMainMenu().addSubMenu("Color Options")
-# btnGrad1 = ss3.addButton("Hour Gradient 1", "setColorBy(0)")
-# btnGrad2 = ss3.addButton("Hour Gradient 2", "setColorBy(1)")
-# btnGrad3 = ss3.addButton("Hour Gradient 3", "setColorBy(2)")
-# btnGrad4 = ss3.addButton("Day Gradient 1", "setColorBy(3)")
-# btnGrad5 = ss3.addButton("Day Gradient 2", "setColorBy(4)")
-# btnGrad6 = ss3.addButton("Color by individual", "setColorBy(5)")
+ss3 = mm.getMainMenu().addSubMenu("Color Options")
+btnGrad1 = ss3.addButton("Hour Gradient 1", "setColorBy(0)")
+btnGrad2 = ss3.addButton("Hour Gradient 2", "setColorBy(1)")
+btnGrad3 = ss3.addButton("Hour Gradient 3", "setColorBy(2)")
+btnGrad4 = ss3.addButton("Day Gradient 1", "setColorBy(3)")
+btnGrad5 = ss3.addButton("Day Gradient 2", "setColorBy(4)")
+btnGrad6 = ss3.addButton("Color by individual", "setColorBy(5)")
 
 # ss4 = mm.getMainMenu().addSubMenu("selected Individual 1")
 # #ss4.setStyleValue('fill', '#954FEA')
@@ -618,79 +575,78 @@ setUpdateFunction(onUpdate)
 
 #--------------------------------------------------------------------------------------
 #Functions
+def oneDayStepUp(value):
+    global myStartDay
+    global myEndDay
+    global numberOfDays
 
-# def oneDayStepUp(value):
-#     global myStartDay
-#     global myEndDay
-#     global numberOfDays
+    myStartDay = myStartDay + 1
+    if myStartDay > numberOfDays:
+        myStartDay = 0
+    myEndDay = myStartDay + 1
+    endDay.setInt(myEndDay)
+    startDay.setInt(myStartDay)
 
-#     myStartDay = myStartDay + 1
-#     if myStartDay > numberOfDays:
-#         myStartDay = 0
-#     myEndDay = myStartDay + 1
-#     endDay.setInt(myEndDay)
-#     startDay.setInt(myStartDay)
+    # print( "one day step up" + myStartDay)
 
-#     # print( "one day step up" + myStartDay)
+def oneDayStepDown(value):
+    global myStartDay
+    global myEndDay
+    global numberOfDays
 
-# def oneDayStepDown(value):
-#     global myStartDay
-#     global myEndDay
-#     global numberOfDays
+    myStartDay = myStartDay - 1
+    if myStartDay < 0:
+        myStartDay = numberOfDays-1
+    myEndDay = myStartDay + 1
+    endDay.setInt(myEndDay)
+    startDay.setInt(myStartDay)
 
-#     myStartDay = myStartDay - 1
-#     if myStartDay < 0:
-#         myStartDay = numberOfDays-1
-#     myEndDay = myStartDay + 1
-#     endDay.setInt(myEndDay)
-#     startDay.setInt(myStartDay)
+    # print( "one day step down " + myStartDay)
 
-#     # print( "one day step down " + myStartDay)
+def sevenDayStepUp(value):
+    global myStartDay
+    global myEndDay
+    global numberOfDays
 
-# def sevenDayStepUp(value):
-#     global myStartDay
-#     global myEndDay
-#     global numberOfDays
+    myStartDay = myStartDay + 7
+    if myStartDay > numberOfDays:
+        myStartDay = 0
+    myEndDay = myStartDay + 7
+    endDay.setInt(myEndDay)
+    startDay.setInt(myStartDay)
 
-#     myStartDay = myStartDay + 7
-#     if myStartDay > numberOfDays:
-#         myStartDay = 0
-#     myEndDay = myStartDay + 7
-#     endDay.setInt(myEndDay)
-#     startDay.setInt(myStartDay)
+    # print( "seven day step up" + myStartDay)
 
-#     # print( "seven day step up" + myStartDay)
+def sevenDayStepDown(value):
+    global myStartDay
+    global myEndDay
+    global numberOfDays
 
-# def sevenDayStepDown(value):
-#     global myStartDay
-#     global myEndDay
-#     global numberOfDays
-
-#     myStartDay = myStartDay - 7
-#     if myStartDay < 0:
-#         myStartDay = numberOfDays-7
-#     myEndDay = myStartDay + 7
-#     endDay.setInt(myEndDay)
-#     startDay.setInt(myStartDay)
+    myStartDay = myStartDay - 7
+    if myStartDay < 0:
+        myStartDay = numberOfDays-7
+    myEndDay = myStartDay + 7
+    endDay.setInt(myEndDay)
+    startDay.setInt(myStartDay)
 
     # print( "seven day step down " + myStartDay)
 
-# def allDay(value):
-#     global numberOfDays
-#     endDay.setInt(numberOfDays)
-#     startDay.setInt(0)
+def allDay(value):
+    global numberOfDays
+    endDay.setInt(numberOfDays)
+    startDay.setInt(0)
 
-#     # print( "one day step " + myStartDay)
+    # print( "one day step " + myStartDay)
 
-# def setColorBy(value):
-#     colorBy.setInt(value)
+def setColorBy(value):
+    colorBy.setInt(value)
 
-#     # print( "set color by " + value)
+    # print( "set color by " + value)
 
-# def setSelIn1(value):
-#     colorBy.setInt(value)
+def setSelIn1(value):
+    colorBy.setInt(value)
 
-#     # print( "set color by " + value)
+    # print( "set color by " + value)
 
 def onPointSizeSliderValueChanged(value):
     if (value != 0):
